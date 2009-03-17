9f3b89db26e47149b1203f88b5c3fa3793524658
Leon Brocard <acme@astray.com>
2009-03-17 08:28:36 +0000

    and release 0.40

--- lib/Net/Mosso/CloudFiles.pm
+++ lib/Net/Mosso/CloudFiles.pm
@@ -8,7 +8,7 @@ use Net::Mosso::CloudFiles::Object;
 use LWP::ConnCache::MaxKeepAliveRequests;
 use LWP::UserAgent::Determined;
 use URI::QueryParam;
-our $VERSION = '0.35';
+our $VERSION = '0.40';
 
 my $DEBUG = 0;
 
