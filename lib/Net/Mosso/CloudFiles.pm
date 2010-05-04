13a958885d6ccda984f2c5c13ffb9b606fd3d338
Htbaa <c.kras@pcc-online.net>
2010-05-04 21:11:12 +0200

    Upped version number to 0.44

--- lib/Net/Mosso/CloudFiles.pm
+++ lib/Net/Mosso/CloudFiles.pm
@@ -8,7 +8,7 @@ use Net::Mosso::CloudFiles::Object;
 use LWP::ConnCache::MaxKeepAliveRequests;
 use LWP::UserAgent::Determined;
 use URI::QueryParam;
-our $VERSION = '0.43';
+our $VERSION = '0.44';
 
 my $DEBUG = 0;
 
