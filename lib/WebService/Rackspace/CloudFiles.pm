ce9d5cd477141167c4aebb1f6fb72887a6e5cbbd
Khaled Hussein <khaled.hussein@gmail.com>
2011-09-14 16:27:36 -0500

    Fixes a bug in returning containers.
    The list of containers are returned from the Storage URI instead of the CDN URI
    since it is the authority on the actual data.
    
    Fixes boolean values in the response, IF list of containers were returned from
    the CDN URI.

--- lib/WebService/Rackspace/CloudFiles.pm
+++ lib/WebService/Rackspace/CloudFiles.pm
@@ -106,7 +106,7 @@ sub _request {
 
 sub containers {
     my $self    = shift;
-    my $request = HTTP::Request->new( 'GET', $self->cdn_management_url . '?format=json',
+    my $request = HTTP::Request->new( 'GET', $self->storage_url . '?format=json',
         [ 'X-Auth-Token' => $self->token ] );
     my $response = $self->_request($request);
     return if $response->code == 204;
@@ -134,6 +134,11 @@ sub container {
     my ( $self, %conf ) = @_;
     confess 'Missing name' unless $conf{name};
     $conf{cloudfiles} = $self;
+    for (keys %conf){
+        if (ref $conf{$_} eq 'JSON::XS::Boolean'){
+            $conf{$_} = ($conf{$_} ? 'true' : 'false');
+        }
+    }
 
     return WebService::Rackspace::CloudFiles::Container->new(%conf);
 }
